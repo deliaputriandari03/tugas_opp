de7f735f5048c2c1b7e83cdcad39cb422b0aa7e4
deliaputriandari03 <103434131+deliaputriandari03@users.noreply.github.com>
2022-04-12 14:41:41 +0700

    Add files via upload

---
+++ oop-pygame-simplegame/main.py
@@ -0,0 +1,32 @@
+import pygame
+from oop.bullets.arrow import Arrow
+from oop.control import GameController
+from oop.scene import Scene
+from oop.levels.level1 import Level1
+from oop.levels.level2 import Level2
+from oop.levels.level3 import Level3
+from oop.levels.level4 import Level4
+from oop.levels.level5 import Level5
+from oop.player import Player
+
+pygame.init()
+
+scene = Scene((640, 480))
+scene.setup_level(
+  Level1,
+  Level2,
+  Level3,
+  Level4,
+  Level5,
+)
+
+player = Player(scene, Arrow())
+game_controller = GameController(player)
+
+running = True
+
+while(running):
+  scene.fill()
+  game_controller.keyboard_event()
+  game_controller.mouse_position_event()
+  pygame.display.flip()
